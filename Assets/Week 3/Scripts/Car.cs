cfa83aae67ba1a1fe2c9ea3730d5ff2ba8c4f91a
Jack <Maclean.Mealey@sheridancollage.ca>
2024-01-24 14:44:02 -0500

    Task 1, 2, and 3 done. Tile map and car physics.

---
+++ Assets/Week 3/Scripts/Car.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car : MonoBehaviour
+{
+    float acceleration;
+    float steering;
+    public float forwardSpeed = 500;
+    public float steeringSpeed = 100;
+    public float maxSpeed = 500;
+    Rigidbody2D rigidbody;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        acceleration = Input.GetAxis("Vertical");
+        steering = Input.GetAxis("Horizontal");
+    }
+
+    private void FixedUpdate()
+    {
+        rigidbody.AddTorque(steering * -steeringSpeed * Time.deltaTime);
+        Vector2 force = transform.up * acceleration * forwardSpeed * Time.deltaTime;
+        if(rigidbody.velocity.magnitude < maxSpeed)
+        {
+            rigidbody.AddForce(force);
+        }
+        
+    }
+}
